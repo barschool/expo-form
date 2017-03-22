dc8b661b241dec9eb253c8c469b46f2fea4f0b9c
Peter <peter@barschool.net>
2017-03-22 10:08:54 +0700

    Moved connection details to config file Using deploy key on prod

--- flightplan.js
+++ flightplan.js
@@ -1,11 +1,13 @@
 var plan = require('flightplan');
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('flightplan.config.json', 'utf8'));
 
 // configuration
 plan.target('prod', [
   {
-    host: 'ebs-platform.com',
-    username: 'peter',
-    privateKey: '/Users/Peter/.ssh/eval_rsa',
+    host: config.prod.host,
+    username: config.prod.username,
+    privateKey: config.prod.privateKey,
     agent: process.env.SSH_AUTH_SOCK
   }
 ]);
@@ -14,7 +16,7 @@ var deploy = true;
 
 // run commands on localhost
 plan.local(function(local) {
-  var result = local.exec('git push https://24b756656f283fc956e7ad9008d4bcba38a57ec0@github.com/barschool/expo-form.git master');
+  var result = local.exec('git push https://'+ config.prod.token +'@github.com/barschool/expo-form.git master');
   if ( result.stderr.search(/Everything up-to-date/m) === 0 ){
     deploy = false;
     local.log('Remote already up-to-date..');
@@ -25,7 +27,7 @@ plan.local(function(local) {
 plan.remote(function(remote) {
   if(deploy){
     remote.log('Pulling master..');
-    remote.exec('git pull https://24b756656f283fc956e7ad9008d4bcba38a57ec0@github.com/barschool/expo-form.git');
+    remote.exec('cd /var/www/html/expo && git pull origin master');
   } else {
     remote.log('Nothing to deploy..');
   }
